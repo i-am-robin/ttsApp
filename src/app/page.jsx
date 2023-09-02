29e139bcf6f606bf29791db4acca2b7a56b59dc3
i-am-robin <developerrobin@gmail.com>
2023-09-02 07:34:45 +0600

    Initial commit from Create Next App

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  const rquest = () => {};
+  return (
+    <main>
+      <button className="py-2 px-3 rounded-md bg-slate-400 text-white">
+        Text
+      </button>
+    </main>
+  );
+}
