076437a133c08fab81ad76c1837f9d2cdfb99b21
i-am-robin <developerrobin@gmail.com>
2023-09-09 08:01:49 +0600

    update 3

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,11 +1,8 @@
 import { NextResponse } from "next/server";
 import fs from "fs";
 
-export async function PUT(req) {
-  const body = await req.json();
-  const { text } = body;
-
-  await fs.writeFileSync("public/a.txt", text);
+export async function GET(req) {
+  await fs.writeFileSync("public/a.txt", Date.now());
 
   const txt = fs.readFileSync("public/a.txt", "utf-8");
   console.log(txt);
